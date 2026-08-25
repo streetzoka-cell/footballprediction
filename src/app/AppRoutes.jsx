12ee7eb70eb81aa43f88fbd8dfa048372f9453d5
streetzoka-cell <streetzoka@gmail.com>
2026-08-26 00:57:38 +0300

    dd

--- src/app/AppRoutes.jsx
+++ src/app/AppRoutes.jsx
@@ -1,11 +1,9 @@
 import { Suspense, lazy } from "react";
 import { Routes, Route } from "react-router-dom";
 import { ROUTES, STUDIO_ROUTES } from "../utils/routes";
-
 import { AdminRoute } from "./guards";
 
-// --- Lazy Page Imports ---
-// Only importing files that we KNOW exist in your project.
+// --- Lazy Page Imports (Mapped exactly to your directory tree) ---
 
 const Home = lazy(() => import("../pages/Home"));
 const Fixtures = lazy(() => import("../pages/Fixtures"));
@@ -38,6 +36,11 @@ const FAQ = lazy(() => import("../pages/FAQ"));
 const HelpCenter = lazy(() => import("../pages/HelpCenter"));
 const NotFound = lazy(() => import("../pages/NotFound"));
 const FootballKnowledge = lazy(() => import("../pages/FootballKnowledge"));
+const Developers = lazy(() => import("../pages/Developers"));
+
+// System Pages
+const Changelog = lazy(() => import("../pages/system/Changelog"));
+const Status = lazy(() => import("../pages/system/Status"));
 
 // Studio Pages
 const ReactorStudio = lazy(() => import("../studio/pages/ReactorStudio"));
@@ -92,6 +95,7 @@ export default function AppRoutes() {
         <Route path={ROUTES.FAQ} element={<FAQ />} />
         <Route path={ROUTES.HELP} element={<HelpCenter />} />
         <Route path={ROUTES.FOOTBALL_KNOWLEDGE} element={<FootballKnowledge />} />
+        <Route path="/developers" element={<Developers />} />
         
         <Route path={STUDIO_ROUTES.HOME} element={<StudioHome />} />
         <Route path={STUDIO_ROUTES.TEMPLATES} element={<StudioTemplates />} />
@@ -100,6 +104,9 @@ export default function AppRoutes() {
         <Route path={STUDIO_ROUTES.WEB_SHOWCASE} element={<WebShowcaseStudio />} />
         <Route path={STUDIO_ROUTES.MEDIA} element={<MediaStudio />} />
         <Route path={STUDIO_ROUTES.FACE_AR} element={<FaceARStudio />} />
+        
+        <Route path={ROUTES.CHANGELOG} element={<Changelog />} />
+        <Route path={ROUTES.STATUS} element={<Status />} />
         
         <Route path={ROUTES.LOGIN} element={<Login />} />
         <Route path={ROUTES.PROFILE} element={<Profile />} />
